56ccbb95777887747f9c40ec9b389fb3f8ef86b1
NoThrottle <ntyt.em@gmail.com>
2024-01-03 20:39:04 +0800

    verison bump

--- MVVM_GMI/Services/ILauncherProperties.cs
+++ MVVM_GMI/Services/ILauncherProperties.cs
@@ -6,13 +6,13 @@
         /// <summary>
         /// Current version of the installed Launcher
         /// </summary>
-        internal static int LauncherVersion { get; } = 3;
+        internal static int LauncherVersion { get; } = 4;
 
 
         /// <summary>
         /// Current version of the installed Launcher in a readable format
         /// </summary>
-        internal static String LauncherVersionReadable { get; } = "1.0.1";
+        internal static String LauncherVersionReadable { get; } = "1.0.2";
 
         /// <summary>
         /// Current version of the config file. 
